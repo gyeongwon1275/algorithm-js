b339fd028244c8658fc942a671b8f6b36ba29c9b
kim <rlaruddnjs28@gmail.com>
2022-01-31 11:19:52 +0900

    Add insertSort

---
+++ sort/insertSort.js
@@ -0,0 +1,21 @@
+const insertSort = (array) => {
+  for (let i = 0; i < array.length; ++i) {
+    let pointer = i;
+
+    while (pointer > 0 && array[pointer] < array[pointer - 1]) {
+      const temp = array[pointer];
+      array[pointer] = array[pointer - 1];
+      array[pointer - 1] = temp;
+      pointer--;
+
+      console.log(`in result=${array}`);
+    }
+    console.log(`out result=${array}`);
+  }
+
+  return array;
+};
+
+module.exports = {
+  insertSort,
+};
