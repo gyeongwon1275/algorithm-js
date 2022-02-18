e99be28c76195c54736009c9b98625cd5cb44272
kim <rlaruddnjs28@gmail.com>
2022-02-18 15:23:41 +0900

    Update: LinkedList

--- dataStructure/LinkedList.ts
+++ dataStructure/LinkedList.ts
@@ -1,66 +1,134 @@
 class ListNode {
-  private data: number;
+  private element: number;
   private next: ListNode;
 
-  constructor(data: number) {
-    this.data = data;
+  constructor(element: number) {
+    this.element = element;
+    this.next = null;
   }
-
   public getData() {
-    return this.data;
+    return this.element;
   }
-
   public getNextOrNull() {
     return this.next;
   }
-
   public setNext(node: ListNode) {
     this.next = node;
   }
 }
 
-class LinkedList extends ListNode {
-  constructor(data?) {
-    super(data);
-  }
-  public static append(rootOrNull: ListNode, data): ListNode {
-    const node = new LinkedList();
+class LinkedList {
+  public static append(rootOrNull: ListNode, element: number) {
+    const node = new ListNode(element);
 
-    return null;
-  }
+    if (rootOrNull) {
+      let current = rootOrNull;
+      while (current.getNextOrNull()) {
+        current = current.getNextOrNull();
+      }
 
-  public static prepend(rootOrNull: ListNode, data): ListNode {
-    return null;
+      current.setNext(node);
+      return rootOrNull;
+    } else {
+      return node;
+    }
   }
-
-  public static insertAt(
-    rootOrNull: ListNode,
-    index: number,
-    data: number
-  ): ListNode {
-    return null;
+  public static prepend(rootOrNull: ListNode, element: number) {
+    const node = new ListNode(element);
+    node.setNext(rootOrNull);
+    return node;
   }
+  public static insertAt(rootOrNull: ListNode, index: number, element: number) {
+    const node = new ListNode(element);
 
-  public static removeAt(rootOrNull: ListNode, index: number): ListNode {
-    return null;
+    if (rootOrNull === null) {
+      return node;
+    } else if (index === 0) {
+      node.setNext(rootOrNull);
+      return node;
+    } else {
+      let current = rootOrNull;
+
+      let position = index;
+      while (position !== 0 && current) {
+        current = current.getNextOrNull();
+
+        position--;
+      }
+
+      rootOrNull.setNext(node);
+      node.setNext(current);
+      return rootOrNull;
+    }
   }
+  public static removeAt(rootOrNull: ListNode, index: number) {
+    if (rootOrNull === null) {
+      return rootOrNull;
+    } else if (index === 0) {
+      return rootOrNull.getNextOrNull();
+    } else if (index < 0) {
+      return null;
+    } else {
+      let position = index;
+      let front = rootOrNull;
+      let back = rootOrNull.getNextOrNull();
 
-  public static getIndexOf(rootOrNull: ListNode, data: number): number {
-    return -1;
+      while (position > 1 && back) {
+        front = front.getNextOrNull();
+        back = back.getNextOrNull();
+        position--;
+      }
+
+      front.setNext(back.getNextOrNull());
+
+      return rootOrNull;
+    }
   }
+  public static getIndexOf(rootOrNull: ListNode, element: number) {
+    if (rootOrNull === null) {
+      return null;
+    } else {
+      let current = rootOrNull;
+      let index = 0;
+      while (current && element !== current.getData()) {
+        current = current.getNextOrNull();
+        index++;
+      }
 
-  public static getOrNull(rootOrNull: ListNode, index: number): ListNode {
-    return null;
+      return current ? index : null;
+    }
   }
+  public static getOrNull(rootOrNull: ListNode, index: number) {
+    if (rootOrNull === null || index < 0) {
+      return null;
+    } else {
+      let current = rootOrNull;
+      let pointer = index;
+      while (pointer !== 0 && current) {
+        current = current.getNextOrNull();
+        pointer--;
+      }
 
-  public static reverse(rootOrNull: ListNode): ListNode {
-    return null;
+      return current || null;
+    }
   }
+  public static reverse(rootOrNull: ListNode) {
+    if (rootOrNull) {
+      let current = rootOrNull;
+      let next = rootOrNull.getNextOrNull();
+      let result = current;
+      result.setNext(null);
 
-  public static interleaveOrNull(
-    rootOrNull: ListNode,
-    root1OrNull: ListNode
-  ): ListNode {
+      while (next) {
+        current = next;
+        next = next.getNextOrNull();
+        current.setNext(result);
+        result = current;
+      }
+
+      return result;
+    }
+
     return null;
   }
 }
--- dataStructure/__tests__/LinkedList.test.js
+++ dataStructure/__tests__/LinkedList.test.js
@@ -1,20 +1,149 @@
 const LinkedList = require("../LinkedList");
 
 describe("LinkedList", () => {
-  it("append Node", () => {
-    let root = LinkedList.append(null, 10);
+  describe("append", () => {
+    it("데이터를 list 맨 뒤에 삽입합니다.", () => {
+      let root = LinkedList.append(null, 10);
+      root = LinkedList.append(root, 11);
+      root = LinkedList.append(root, 12);
 
-    root = LinkedList.append(root, 11);
-    root = LinkedList.append(root, 12);
+      expect(root.getData()).toBe(10);
 
-    expect(root.getData()).toBe(10);
+      let next = root.getNextOrNull();
+      expect(next.getData()).toBe(11);
 
-    let next = root.getNextOrNull();
+      next = next.getNextOrNull();
+      expect(next.getData()).toBe(12);
+    });
+  });
 
-    expect(next.getData()).toBe(11);
+  describe("prepend", () => {
+    it("데이터를 list 맨 앞에 삽입합니다.", () => {
+      let root = LinkedList.append(null, 10);
 
-    next = next.getNextOrNull();
+      root = LinkedList.prepend(root, 11);
 
-    expect(next.getData()).toBe(12);
+      expect(root.getData()).toBe(11);
+
+      root = LinkedList.prepend(root, 12);
+
+      expect(root.getData()).toBe(12);
+
+      let next = root.getNextOrNull();
+
+      expect(next.getData()).toBe(11);
+
+      next = next.getNextOrNull();
+
+      expect(next.getData()).toBe(10);
+    });
+  });
+
+  describe("insertAt", () => {
+    it("데이터를 특정위치에 삽입합니다.", () => {
+      let root = LinkedList.append(null, 10);
+
+      root = LinkedList.insertAt(root, 0, 11);
+      expect(root.getData()).toBe(11);
+
+      root = LinkedList.insertAt(root, 1, 12);
+      expect(root.getData()).toBe(11);
+
+      let next = root.getNextOrNull();
+      expect(next.getData()).toBe(12);
+
+      next = next.getNextOrNull();
+      expect(next.getData()).toBe(10);
+    });
+  });
+
+  describe("removeAt", () => {
+    it("특정 위치의 데이터를 제거합니다.", () => {
+      let root = LinkedList.append(null, 10);
+
+      root = LinkedList.append(root, 11);
+      root = LinkedList.append(root, 12);
+      root = LinkedList.append(root, 13);
+      root = LinkedList.append(root, 14);
+      root = LinkedList.append(root, 15);
+
+      root = LinkedList.removeAt(root, 0);
+
+      expect(root.getData()).toBe(11);
+
+      root = LinkedList.removeAt(root, 1);
+
+      expect(root.getData()).toBe(11);
+
+      const next = root.getNextOrNull();
+
+      expect(next.getData()).toBe(13);
+
+      root = LinkedList.removeAt(root, 2);
+      expect(root.getNextOrNull().getNextOrNull().getData()).toBe(15);
+
+      root = LinkedList.removeAt(root, -2);
+      expect(root).toBeNull();
+    });
+  });
+
+  describe("getIndexOf", () => {
+    it("특정 element 가 전체 연결리스트에서 몇번째 있는지 확인", () => {
+      let root = LinkedList.append(null, 10);
+      root = LinkedList.append(root, 11);
+      root = LinkedList.append(root, 12);
+      root = LinkedList.append(root, 13);
+      root = LinkedList.append(root, 14);
+      root = LinkedList.append(root, 15);
+
+      expect(LinkedList.getIndexOf(null, 10)).toBe(null);
+      expect(LinkedList.getIndexOf(root, 10)).toBe(0);
+      expect(LinkedList.getIndexOf(root, 11)).toBe(1);
+      expect(LinkedList.getIndexOf(root, 12)).toBe(2);
+      expect(LinkedList.getIndexOf(root, 13)).toBe(3);
+      expect(LinkedList.getIndexOf(root, 14)).toBe(4);
+      expect(LinkedList.getIndexOf(root, 15)).toBe(5);
+      expect(LinkedList.getIndexOf(root, 1121)).toBe(null);
+    });
+  });
+
+  describe("getOrNull", () => {
+    it("연결리스트 특정 위치의 있는 Node 확인", () => {
+      let root = LinkedList.append(null, 10);
+      root = LinkedList.append(root, 11);
+      root = LinkedList.append(root, 12);
+      root = LinkedList.append(root, 13);
+      root = LinkedList.append(root, 14);
+      root = LinkedList.append(root, 15);
+
+      expect(LinkedList.getOrNull(null, 0)).toBe(null);
+      expect(LinkedList.getOrNull(root, 0).getData()).toBe(10);
+      expect(LinkedList.getOrNull(root, 1).getData()).toBe(11);
+      expect(LinkedList.getOrNull(root, 2).getData()).toBe(12);
+      expect(LinkedList.getOrNull(root, 3).getData()).toBe(13);
+      expect(LinkedList.getOrNull(root, 4).getData()).toBe(14);
+      expect(LinkedList.getOrNull(root, 5).getData()).toBe(15);
+      expect(LinkedList.getOrNull(root, 6)).toBe(null);
+    });
+  });
+
+  describe("reverse", () => {
+    it("연결리스트를 뒤집습니다.", () => {
+      let root = LinkedList.append(null, 10);
+      root = LinkedList.append(root, 11);
+      root = LinkedList.append(root, 12);
+      root = LinkedList.append(root, 13);
+      root = LinkedList.append(root, 14);
+      root = LinkedList.append(root, 15);
+
+      root = LinkedList.reverse(root);
+
+      expect(LinkedList.getOrNull(root, 0).getData()).toBe(15);
+      expect(LinkedList.getOrNull(root, 1).getData()).toBe(14);
+      expect(LinkedList.getOrNull(root, 2).getData()).toBe(13);
+      expect(LinkedList.getOrNull(root, 3).getData()).toBe(12);
+      expect(LinkedList.getOrNull(root, 4).getData()).toBe(11);
+      expect(LinkedList.getOrNull(root, 5).getData()).toBe(10);
+    });
   });
 });
