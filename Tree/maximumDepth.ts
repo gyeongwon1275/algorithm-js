7aacafa2a75ac504ce6b0a7c382851d7d7c76e92
gyeongwon1275 <rlaruddnjs28@gmail.com>
2022-03-08 14:09:34 +0900

    Typo: annotation

--- Tree/maximumDepth.ts
+++ Tree/maximumDepth.ts
@@ -39,6 +39,20 @@ function maxDepth(root) {
 } */
 
 // using BFS
+
+/* 
+
+각 depth 마다 반복문을 별도로 가져갈 수 있으면 
+depth 가 바뀔 때마다 depth 를 1씩 증가시키고 
+최종 depth 를 반환하면 됨 
+
+전체 depth 를 관리하는 반복문 안에 
+자식 node 를 queue 에 넣는 반복문을 둠 
+
+queue 길이를 미리 받아놓고 queue 길이만큼 
+자식 node 를 넣음 
+
+*/
 function maxDepth(root) {
   if (!root) {
     return 0;
