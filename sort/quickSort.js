488ac74979a7a081156fc71adc9fc096578841b1
kim <rlaruddnjs28@gmail.com>
2022-03-13 17:18:13 +0900

    Add: right pivot quick sort

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -26,6 +26,38 @@ const quickSort = (array) => {
   return quickSortRecursive(0, array.length - 1, array);
 };
 
+/* 
+
+pivot 이 right 버젼
+const quickSortRecursive = (array, left = 0, pivot = array.length - 1) => {
+  if (left >= pivot) {
+    return;
+  }
+
+  let leftTemp = left;
+
+  for (let current = left; current < pivot; current++) {
+    if (array[current] <= array[pivot]) {
+      const temp = array[current];
+      array[current] = array[leftTemp];
+      array[leftTemp] = temp;
+      leftTemp++;
+    }
+  }
+
+  const temp = array[leftTemp];
+  array[leftTemp] = array[pivot];
+  array[pivot] = temp;
+
+  quickSortRecursive(array, left, leftTemp - 1);
+  quickSortRecursive(array, leftTemp + 1, pivot);
+};
+
+const quickSort = (array) => {
+  return quickSortRecursive(array, 0, array.length - 1);
+};
+*/
+
 module.exports = {
   quickSort,
 };
