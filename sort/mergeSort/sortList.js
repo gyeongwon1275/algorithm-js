6b67bbdd82426c616c8dc4be2113cd185dc7e438
gyeongwon1275 <rlaruddnjs28@gmail.com>
2022-03-14 09:32:52 +0900

    Update: 배열사용하지 않는 버전 추가

--- sort/mergeSort/sortList.js
+++ sort/mergeSort/sortList.js
@@ -1,49 +1,12 @@
+const { mergeSort } = require("./mergeSort");
+const { mergeTwoLists } = require("./mergeTwoSortedList");
+
 function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
 }
 
-const mergeSortedArray = (left, right) => {
-  let leftIndex = 0;
-  let rightIndex = 0;
-
-  const result = [];
-
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] <= right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
-    }
-  }
-
-  if (leftIndex < left.length) {
-    result.push(...left.slice(leftIndex));
-  }
-
-  if (rightIndex < right.length) {
-    result.push(...right.slice(rightIndex));
-  }
-
-  return result;
-};
-
-const mergeSort = (array) => {
-  if (array.length <= 1) {
-    return array;
-  }
-
-  const middleIndex = Math.round(array.length * 0.5);
-
-  const left = mergeSort(array.slice(0, middleIndex));
-  const right = mergeSort(array.slice(middleIndex));
-
-  return mergeSortedArray(left, right);
-};
-
-var sortList = function (head) {
+/* var sortList = function (head) {
   if (!head) {
     return head;
   }
@@ -67,6 +30,38 @@ var sortList = function (head) {
   }
 
   return result;
+}; */
+
+const sortList = function (head) {
+  // 완벽하게 반을 나누려면 list 전체를 확인해야 함
+  // pointer 를 3개 사용해서 대략적으로 나눌 수 있다.
+
+  // half
+  // slow
+  // fast
+
+  if (!head || !head.next) {
+    return head;
+  }
+
+  let half = null;
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    half = slow;
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  // head 를  반띵 전까지 로 분리
+  // slow 는 반찡 이후 linkedList
+  half.next = null;
+
+  const list1 = sortList(head);
+  const list2 = sortList(slow);
+
+  return mergeTwoLists(list1, list2);
 };
 
 module.exports = { sortList, ListNode };
