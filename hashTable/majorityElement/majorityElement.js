703a852d68c2527d4f456c363c53cfa5b13b3564
kim <rlaruddnjs28@gmail.com>
2022-03-19 09:21:12 +0900

    Update: majorityElement
    - 시간복잡도 O(n) 으로 줄임

--- hashTable/majorityElement/majorityElement.js
+++ hashTable/majorityElement/majorityElement.js
@@ -3,20 +3,23 @@
  * @return {number}
  */
 const majorityElement = function (nums) {
+  if (nums.length < 2) {
+    return nums[0];
+  }
+
   const numCount = {};
 
   for (let i = 0; i < nums.length; i++) {
     if (!numCount[nums[i]]) {
       numCount[nums[i]] = 1;
-    } else {
+    } else if (numCount[nums[i]]) {
       numCount[nums[i]] = numCount[nums[i]] + 1;
+
+      if (numCount[nums[i]] > nums.length * 0.5) {
+        return Number(nums[i]);
+      }
     }
   }
-
-  const [num] = Object.entries(numCount).find(
-    ([, count]) => count > nums.length * 0.5
-  );
-  return Number(num);
 };
 
 module.exports = majorityElement;
