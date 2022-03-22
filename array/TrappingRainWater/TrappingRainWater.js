c0f464209bd14c427790e52031f93a49ee1e2690
gyeongwon1275 <rlaruddnjs28@gmail.com>
2022-03-22 11:07:52 +0900

    Update: twopointer 사용버전 추가

--- array/TrappingRainWater/TrappingRainWater.js
+++ array/TrappingRainWater/TrappingRainWater.js
@@ -1,4 +1,4 @@
-const trap = function (height) {
+/* const trap = function (height) {
   let leftMaxHeight = height[0];
 
   let trappedWater = 0;
@@ -18,6 +18,44 @@ const trap = function (height) {
     if (leftMaxHeight > height[i] && rightMaxHeight > height[i]) {
       trappedWater =
         trappedWater + (Math.min(rightMaxHeight, leftMaxHeight) - height[i]);
+    }
+  }
+
+  return trappedWater;
+}; */
+
+/* 
+
+TWO Pointer
+최대높이에 도달하기 전까지 과거 지나왔던 높이의 최대값 - 현재높이 만큼 
+부피에 추가하면 됨 
+
+과거 지나왔던 높이의 최대값 - 현재높이 만큼 
+부피에 추가하는 과정을 왼쪽, 오른쪽 각각 최대값에 도달하기 전까지 하면 됨 
+
+*/
+
+const trap = function (height) {
+  let leftPosition = 0;
+  let rightPosition = height.length - 1;
+
+  let leftMax = height[leftPosition];
+  let rightMax = height[rightPosition];
+
+  let trappedWater = 0;
+
+  while (leftPosition < rightPosition) {
+    leftMax = Math.max(leftMax, height[leftPosition]);
+    rightMax = Math.max(rightMax, height[rightPosition]);
+
+    if (leftMax <= rightMax) {
+      trappedWater = trappedWater + leftMax - height[leftPosition];
+
+      leftPosition++;
+    } else {
+      trappedWater = trappedWater + rightMax - height[rightPosition];
+
+      rightPosition--;
     }
   }
 
