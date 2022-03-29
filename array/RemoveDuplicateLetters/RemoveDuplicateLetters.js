2a993f6fbebef427953576c9ac1d9bfb8b3d5555
gyeongwon1275 <rlaruddnjs28@gmail.com>
2022-03-29 09:40:57 +0900

    Update: RemoveDuplicateLetters stack version 추가

--- array/RemoveDuplicateLetters/RemoveDuplicateLetters.js
+++ array/RemoveDuplicateLetters/RemoveDuplicateLetters.js
@@ -18,7 +18,7 @@ char 를 하나하나 넣어가면서 가능한 char 인지 확인하면 됨
 
 */
 
-const removeDuplicateLetters = function (s) {
+/* const removeDuplicateLetters = function (s) {
   const set = [...new Set(s)].sort();
 
   for (let i = 0; i < set.length; i++) {
@@ -35,6 +35,47 @@ const removeDuplicateLetters = function (s) {
   }
 
   return "";
+}; */
+
+/* 
+stack 에 중복없이 사전순으로 빠른 char 를 넣어놓고 마지막에 join 해서 반환 
+
+중복없이 넣으려면 현재 stack 에서 지금 넣으려고 하는 char 가 있는지, 없는지만 확인
+
+사전순으로 빠른 걸 넣으려면 현재 stack 에 있는 모든 요소가 넣으려고 하는 char 보다 아스키 코드가 크면
+
+전부 빼내고 char 를 넣는다. 
+
+현재 stack 에 들어가 있는 요소들의 여유분이 없을경우 못빼고 그냥 stack 위에 넣어야 한다.
+*/
+const removeDuplicateLetters = function (s) {
+  const counter = {};
+
+  s.split("").forEach((char) => {
+    counter[char] = counter[char] ? counter[char] + 1 : 1;
+  });
+
+  const stack = [];
+
+  for (const char of s) {
+    counter[char] = counter[char] ? counter[char] - 1 : 0;
+
+    if (stack.includes(char)) {
+      continue;
+    }
+
+    while (
+      stack.length &&
+      stack[stack.length - 1] > char &&
+      counter[stack[stack.length - 1]] > 0
+    ) {
+      stack.pop();
+    }
+
+    stack.push(char);
+  }
+
+  return stack.join("");
 };
 
 module.exports = removeDuplicateLetters;
